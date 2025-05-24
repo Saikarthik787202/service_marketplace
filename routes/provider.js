936710d90182cc0b270487c31bcb8f85e3e37059
Saikarthik787202 <s225173844@deakin.edu.au>
2025-05-24 18:28:10 +1000

    Add files via upload

--- routes/provider.js
+++ routes/provider.js
@@ -15,8 +15,12 @@ router.get('/revenue', authRole('provider'), providerController.revenue);
 router.post('/orders/:id/accept', authRole('provider'), providerController.acceptOrder);
 router.post('/orders/:id/cancel', authRole('provider'), providerController.cancelOrder);
 
+router.get('/services', authRole('provider'), providerController.listServices);
 router.get('/services/new', authRole('provider'), providerController.getNewService);
 router.post('/services', authRole('provider'), providerController.createService);
-router.post('/services/:id/delete', authRole('provider'), providerController.deleteService);
+router.post('/services/:id/delete', authRole('provider'), providerController.softDeleteService);
+router.get('/services/deleted', authRole('provider'), providerController.deletedServices);
+router.post('/services/:id/restore', authRole('provider'), providerController.restoreService);
+router.post('/services/bulk-edit', authRole('provider'), providerController.bulkEdit);
 
 module.exports = router;
